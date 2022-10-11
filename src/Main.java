d0b74ab779f6fb580eff7168386032a336e2bb40
AsTikiN <77283605+AsTikiN@users.noreply.github.com>
2022-10-11 13:13:27 +0300

    feat():init

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String[] args) {
+        char c2;
+        byte b;
+        float f;
+        int i = -500;
+
+        f = ThreadLocalRandom.current().nextFloat(0, 1000001);
+        System.out.println("random float value: " + f);
+        i = Math.round(f);
+        System.out.println("Rounded float to int value: " + i);
+        b = (byte)i;
+        System.out.println("Converted int to byte value: " + b);
+    }
+}
